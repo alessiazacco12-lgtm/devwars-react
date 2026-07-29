e63d706a8ca1771a61767b3c365de79c18b0c208
alessia <alessia.zacco12@gmail.com>
2026-07-29 16:05:37 +0200

    Aggiorno DevArena.tsx

--- src/components/DevArena.tsx
+++ src/components/DevArena.tsx
@@ -1,28 +1,31 @@
 // 1. IMPORTAZIONI --
 // Importo il tipo Dev dal file types.ts.
-import type {Dev} from "../types";
+import type { Dev } from "../types";
 
 // Importo il componente che mostra la card dello sviluppatore.
 import DevCard from "./DevCard";
 
+// -----------------------------------------------------------------------------------------------------------------------
 // 2. TIPO DELLE PROPS -- Definisce i due sviluppatori ricevuti dal componente App.
-type DevArenaProps ={
+type DevArenaProps = {
     devA: Dev;
     devB: Dev;
 };
 
+// -----------------------------------------------------------------------------------------------------------------------
 // 3. COMPONENTE DEV ARENA -- Mostro i due sviluppatori uno di fronte all'altro.
-function DevArena ({devA,devB}: DevArenaProps){
+function DevArena({ devA, devB }: DevArenaProps) {
     return (
-        <section className= "dev-arena">
+        <section className="dev-arena">
             {/* Mostro la card dello sviluppatore selezionato come Dev A. */}
-            <DevCard dev= {devA} side = "A"/>
+            <DevCard dev={devA} side="A" />
             {/* Mostro  la scritta VS tra i due sviluppatori.*/}
-            <div className = "versus"> VS </div>
+            <div className="versus"> VS </div>
             {/* Mostro la card dello sviluppatore selezionato come DevB. */}
-            <DevCard dev = {devB} side = "B"/>
+            <DevCard dev={devB} side="B" />
         </section>
     );
 }
+// -----------------------------------------------------------------------------------------------------------------------
 // 4. ESPORTAZIONE -- Esporto il componente per utilizzarlo in App.
 export default DevArena;
