81e980f4942e0c16124412b5d03172c40d2fa72b
alessia <alessia.zacco12@gmail.com>
2026-07-29 16:01:44 +0200

    Aggiorno SkillBar.tsx

--- src/components/SkillBar.tsx
+++ src/components/SkillBar.tsx
@@ -1,10 +1,12 @@
 // 1. IMPORTAZIONI -- Importo il tipo Skill dal file types.ts.
 import type {Skill} from "../types";
+
+// -----------------------------------------------------------------------------------------------------------------------
 // 2. TIPO DELLE PROPS -- Definisco i dati ricevuti dal componente SkillBar.
 type SkillBarProps ={
     skill: Skill;
 };
-
+// -----------------------------------------------------------------------------------------------------------------------
 // 3. COMPONENTE SKILL BAR -- Mostro una singola competenza con: nome, tipologia, valore numerico e barra grafica.
 function SkillBar ({skill} : SkillBarProps){
     return(
@@ -33,7 +35,7 @@ function SkillBar ({skill} : SkillBarProps){
     </article>
     );
 }
-
+// -----------------------------------------------------------------------------------------------------------------------
 // 4. ESPORTAZIONE -- Esporto il componente per poi utilizzarlo in DevCard.
 export default SkillBar;
     
