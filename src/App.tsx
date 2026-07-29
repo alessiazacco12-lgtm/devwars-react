592dce4172ad6aa62ca890c893f75aa99f6fcdc4
alessia <alessia.zacco12@gmail.com>
2026-07-29 14:40:22 +0200

    Aggiorno App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,11 @@ import "./styles/selector.css";
 import "./styles/card.css";
 import "./styles/results.css";
 
+// -----------------------------------------------------------------------------------------------------------------------
 // 2. COMPONENTE PRINCIPALE -- 
 function App() {
 
-  // 3. STATI -- Salva lo sviluppatore selezionato come Dev A. Inizialmente viene mostrato il primo sviluppatore dell'array. 
+// 3. STATI -- Salva lo sviluppatore selezionato come Dev A. Inizialmente viene mostrato il primo sviluppatore dell'array. 
   const [selectedDevA, setSelectedDevA] = useState<Dev>(developers[0]);
 
   // Salva lo sviluppatore selezionato come Dev B. Inizialmente viene mostrato il secondo sviluppatore dell'array. 
@@ -28,7 +29,9 @@ function App() {
   // Indica se l'utente sta selezioando il Dev A oppure il Dev B.
   const [activeSelection, setActiveSelection] = useState<SelectionSide>("A");
 
-  // 4. SELEZIONE DELLO SVILUPPATORE --  Riceve lo sviluppatore cliccato e lo assegna a Dev A oppure Dev B in base al pulsante di selezione attivo.
+  // -----------------------------------------------------------------------------------------------------------------------
+
+// 4. SELEZIONE DELLO SVILUPPATORE --  Riceve lo sviluppatore cliccato e lo assegna a Dev A oppure Dev B in base al pulsante di selezione attivo.
   function selectDeveloper(dev: Dev): void {
 
     // Dev A può essere selezionato solo se è maschio.
@@ -38,8 +41,9 @@ function App() {
       setSelectedDevB(dev);
     }
   }
+// -----------------------------------------------------------------------------------------------------------------------
 
-  //  5. TEMPLATE -- Restituisco la struttura principale dell'applicazione.
+//  5. TEMPLATE -- Restituisco la struttura principale dell'applicazione.
   return (
     <main className="main-container">
 
@@ -58,13 +62,15 @@ function App() {
         setActiveSelection={setActiveSelection}
         selectDeveloper={selectDeveloper} />
 
-      {/* Arena che mostra i due sviluppatori uno di fronte all'altro. */}
+      {/* Arena che mostra i due sviluppatori uno accanto all'altro. */}
       <DevArena devA={selectedDevA} devB={selectedDevB} />
 
-      {/* Sezione con i risultati del confronto. */}
+      {/* Sezione con i risultati che scaturiscono dal confronto. */}
       <ComparisonResults devA={selectedDevA} devB={selectedDevB} />
     </main>
   );
 }
+
+// -----------------------------------------------------------------------------------------------------------------------
 // 6. ESPORTAZIONE -- Esporto il componente principale.
 export default App;
