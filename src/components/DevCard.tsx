8343a0c8c5e43832f84be4644b7d53046e2d0dee
alessia <alessia.zacco12@gmail.com>
2026-07-29 16:03:42 +0200

    Aggiorno DevCard.tsx

--- src/components/DevCard.tsx
+++ src/components/DevCard.tsx
@@ -1,87 +1,89 @@
 // 1. IMPORTAZIONI -- Importo i tipi necessari.
-import type {Dev,SelectionSide } from "../types";
+import type { Dev, SelectionSide } from "../types";
 
 // Importo il componente che mostra le competenze.
 import SkillBar from "./SkillBar";
 
 // Importo le funzioni utili per mostrare nome completo, data, genere e le medie.
-import {getFullname,formatBirthDate,getGenderText,getAverageByType,getOverallAverage} from "../functions";
+import { getFullname, formatBirthDate, getGenderText, getAverageByType, getOverallAverage } from "../functions";
 
+// -----------------------------------------------------------------------------------------------------------------------
 // 2. TIPO DELLE PROPS -- Definisco i dati ricevuti dal componente DevCard.
 type DevCardProps = {
     dev: Dev;
     side: SelectionSide;
 };
 
+// -----------------------------------------------------------------------------------------------------------------------
 // 3. COMPONENTE DEV CARD -- Mostro tutte le info di uno sviluppatore. 
-function DevCard ({dev,side}: DevCardProps){
+function DevCard({ dev, side }: DevCardProps) {
 
     // Calcolo la media delle competenze front-end.
-    const frontEndAverage = getAverageByType(dev,"front-end");
+    const frontEndAverage = getAverageByType(dev, "front-end");
     // Calcolo la media delle competenze back-end.
-    const backEndAverage = getAverageByType(dev,"back-end");
+    const backEndAverage = getAverageByType(dev, "back-end");
     // Calcolo la media complessiva di tutte le competenze.
     const overallAverage = getOverallAverage(dev);
-// Restituisco la card completa dello sviluppatore.
-return(
-    <article className = {"dev-card dev-card-" + side.toLowerCase()}>
-{/* Parte sup con immagine e dati personali. */}
-<header className = "dev-card-header">
-    {/* Immagine dello sviluppatore. */}
-    <img src= {dev.imageUrl} alt= {getFullname(dev)}/>
+    // Restituisco la card completa dello sviluppatore.
+    return (
+        <article className={"dev-card dev-card-" + side.toLowerCase()}>
+            {/* Parte sup con immagine e dati personali. */}
+            <header className="dev-card-header">
+                {/* Immagine dello sviluppatore. */}
+                <img src={dev.imageUrl} alt={getFullname(dev)} />
 
-    {/* Etichetta DEV A oppure DEV B. */}
-<span className = {
-    "dev-badge dev-badge-" + side.toLowerCase()
-}>DEV {side}</span>
+                {/* Etichetta DEV A oppure DEV B. */}
+                <span className={
+                    "dev-badge dev-badge-" + side.toLowerCase()
+                }>DEV {side}</span>
 
-{/* Nome, data di nascita e genere. */}
-<div className = "dev-personal-info">
-    <h2> {getFullname(dev)}</h2>
-    <p> Nato/a il {" "} {formatBirthDate(dev.birthDate)}</p>
-    <p> Genere: {" "} {getGenderText(dev.gender)}</p>
-</div>
-</header>
-{/* Parte inf con medie e competenze. */}
-<section className= "dev-card-content">
+                {/* Nome, data di nascita e genere. */}
+                <div className="dev-personal-info">
+                    <h2> {getFullname(dev)}</h2>
+                    <p> Nato/a il {" "} {formatBirthDate(dev.birthDate)}</p>
+                    <p> Genere: {" "} {getGenderText(dev.gender)}</p>
+                </div>
+            </header>
+            {/* Parte inf con medie e competenze. */}
+            <section className="dev-card-content">
 
-{/* Contiene le 3 medie principali. */}
-<div className = "averages">
+                {/* Contiene le 3 medie principali. */}
+                <div className="averages">
 
-    {/* Media front-end */}
-    <article className= "average-box">
-<span>FRONT-END</span>
-<strong>{frontEndAverage.toFixed(1)}</strong>
-</article>
-{/* Media back-end */}
-    <article className= "average-box">
-<span>BACK-END</span>
-<strong>{backEndAverage.toFixed(1)}</strong>
-</article>
-{/* Media complessiva */}
-    <article className= "average-box">
-<span>OVERALL</span>
-<strong>{overallAverage.toFixed(1)}</strong>
-</article>
-</div>
-{/* Elenco delle competenze dello sviluppatore. */}
-<section className= "skills-list">
+                    {/* Media front-end */}
+                    <article className="average-box">
+                        <span>FRONT-END</span>
+                        <strong>{frontEndAverage.toFixed(1)}</strong>
+                    </article>
+                    {/* Media back-end */}
+                    <article className="average-box">
+                        <span>BACK-END</span>
+                        <strong>{backEndAverage.toFixed(1)}</strong>
+                    </article>
+                    {/* Media complessiva */}
+                    <article className="average-box">
+                        <span>OVERALL</span>
+                        <strong>{overallAverage.toFixed(1)}</strong>
+                    </article>
+                </div>
+                {/* Elenco delle competenze dello sviluppatore. */}
+                <section className="skills-list">
 
-    {/* Scorre l'array delle competenze. */}
-    {dev.skills.map((skill) => {
-        // Per ogni competenza crea un componente SkillBar.
-        return ( <SkillBar 
-        // Key combina id e nome della competenza.
-        key ={ dev.id + "-" + skill.label}
-        // Passa la competenza corrente.
-        skill = {skill}/>
+                    {/* Scorre l'array delle competenze. */}
+                    {dev.skills.map((skill) => {
+                        // Per ogni competenza crea un componente SkillBar.
+                        return (<SkillBar
+                            // Key combina id e nome della competenza.
+                            key={dev.id + "-" + skill.label}
+                            // Passa la competenza corrente.
+                            skill={skill} />
+                        );
+                    })}
+                </section>
+            </section>
+        </article>
     );
-    })}
-</section>
-</section>
-</article>
-);
 }
-
+// -----------------------------------------------------------------------------------------------------------------------
 // 4. ESPORTAZIONE -- Esporto il componente per utilizzarlo in DevArena.
 export default DevCard;
