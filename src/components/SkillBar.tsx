7110fd2421d29f654e1a60a025c4c2c98157df13
alessia <alessia.zacco12@gmail.com>
2026-07-30 12:18:34 +0200

    Aggiorno SkillBar.tsx

--- src/components/SkillBar.tsx
+++ src/components/SkillBar.tsx
@@ -4,13 +4,14 @@ import type {Skill} from "../types";
 // -----------------------------------------------------------------------------------------------------------------------
 // 2. TIPO DELLE PROPS -- Definisco i dati ricevuti dal componente SkillBar.
 type SkillBarProps ={
-    skill: Skill;
+    skill: Skill; // Competenza da mostrare.
 };
 // -----------------------------------------------------------------------------------------------------------------------
 // 3. COMPONENTE SKILL BAR -- Mostro una singola competenza con: nome, tipologia, valore numerico e barra grafica.
 function SkillBar ({skill} : SkillBarProps){
     return(
         <article className="skill-item">
+
         {/* Parte sup della competenza */}
     <div className = "skill-info">
 
@@ -23,11 +24,13 @@ function SkillBar ({skill} : SkillBarProps){
     {/* Mostro il valore numerico della competenza. */}
     <strong className = "skill-value"> {skill.value}/100 </strong>
     </div>
+
     {/* Sfondo completo della barra. */}
     <div className = "skill-bar">
 
     {/* Parte colorata della barra.*/}
     <div className = {"skill-progress " + skill.type }
+    
     // La larghezza della barra dipende dal valore della competenza. 
     style= {{width: skill.value + "%" }}>
     </div>
